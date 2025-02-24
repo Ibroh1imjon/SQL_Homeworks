d46c622dc0ed872261ed8316c7fdc9b8b33379b0
Ibroh1imjon <ibrohimjon1909@icloud.com>
2025-02-24 15:01:31 +0500

    Update join.sql

--- lesson-11/homework/join.sql
+++ lesson-11/homework/join.sql
@@ -1,1 +1,8 @@
+select customer_name from Orders o
+join Customers c on o.customer_id = c.customer_id
+group by c.customer_name
+having count(order_id) > (select count(order_id)/count(distinct(customer_id)) from Orders)
 
+select customer_name, sum(total_amount) from Orders o
+join Customers c on o.customer_id = c.customer_id
+group by c.customer_name
